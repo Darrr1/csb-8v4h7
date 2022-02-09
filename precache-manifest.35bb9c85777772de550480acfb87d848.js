3a16ad74f275c054436ea79ddb9033930fcb75ab
Code Sandbox <hello@codesandbox.io>
2022-02-09 21:38:16 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.35bb9c85777772de550480acfb87d848.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65c1a1f0a93cc6ef23200a536502ddcb",
+    "url": "https://darrr1.github.io/csb-8v4h7/index.html"
+  },
+  {
+    "revision": "d63bcad1eaef3d3dcc54",
+    "url": "https://darrr1.github.io/csb-8v4h7/static/css/main.56f1a217.chunk.css"
+  },
+  {
+    "revision": "023f5a0269999fb52107",
+    "url": "https://darrr1.github.io/csb-8v4h7/static/js/2.25fd769d.chunk.js"
+  },
+  {
+    "revision": "d63bcad1eaef3d3dcc54",
+    "url": "https://darrr1.github.io/csb-8v4h7/static/js/main.19074066.chunk.js"
+  },
+  {
+    "revision": "f64e1fc1291e35fd5089",
+    "url": "https://darrr1.github.io/csb-8v4h7/static/js/runtime~main.daa64630.js"
+  },
+  {
+    "revision": "a48640a284521dc4184480c904702eab",
+    "url": "https://darrr1.github.io/csb-8v4h7/static/media/all-programs.a48640a2.ico"
+  },
+  {
+    "revision": "199b5352639f3881890888b0df965beb",
+    "url": "https://darrr1.github.io/csb-8v4h7/static/media/error.199b5352.wav"
+  },
+  {
+    "revision": "cfc00209c2e6a9df522c90e356be6241",
+    "url": "https://darrr1.github.io/csb-8v4h7/static/media/find.cfc00209.svg"
+  },
+  {
+    "revision": "e23232f0ee9089a33025c8a1aa81146b",
+    "url": "https://darrr1.github.io/csb-8v4h7/static/media/narrator.e23232f0.ico"
+  },
+  {
+    "revision": "bfcfd28fbabd92589d0974b2760ed758",
+    "url": "https://darrr1.github.io/csb-8v4h7/static/media/restart.bfcfd28f.ico"
+  },
+  {
+    "revision": "8e8ae78a182de1b6207be2f91244f3e8",
+    "url": "https://darrr1.github.io/csb-8v4h7/static/media/restore.8e8ae78a.ico"
+  },
+  {
+    "revision": "28ff52a81d3d6f9369e199a42b76a73a",
+    "url": "https://darrr1.github.io/csb-8v4h7/static/media/smile.28ff52a8.svg"
+  },
+  {
+    "revision": "415b027d489dfadce00f9e229872e609",
+    "url": "https://darrr1.github.io/csb-8v4h7/static/media/view-info.415b027d.ico"
+  }
+]);
